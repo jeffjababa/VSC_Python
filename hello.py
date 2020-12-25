8d83d4be86a6ddc4162b6e1c1e1f173710d3c57a
Jeff Ding <jeffjababa@hotmail.com>
2020-12-24 20:53:15 -0800

    deleted some code

--- hello.py
+++ hello.py
@@ -4,16 +4,7 @@ from os import rename
 
 import requests
 
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = "hello, {}".format(who_to_greet)
-    return greeting
-
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
-print(greet("World"))
-print(greet("Jeff"))
+print(r.ok)
